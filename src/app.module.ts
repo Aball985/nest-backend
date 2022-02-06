ab224f63008a50a66d84cd38a7c34033c835560c
Abool <arball44@gmail.com>
2022-02-06 00:08:39 -0500

    More changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './models/user.model';
+import { UsersModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'austin22',
+      database: 'nestdb',
+      autoLoadModels: true,
+      synchronize: true,
+      models: [User],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
